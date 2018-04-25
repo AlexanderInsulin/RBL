a8b40b0d1cdc0963dd8f7740c0dff500f7d5c9d8
Alex Insulin <insulin@protonmail.com>
2018-04-25 07:57:17 +0300

    Created wrapper for page.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 // @flow
 import React from "react";
+import styled from "styled-components";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { MainPage } from "./pages";
 import { Header, Player } from "./components";
@@ -8,10 +9,17 @@ const App = () => (
   <Router>
     <div>
       <Header />
-      <Route exact path="/" component={MainPage} />
+      <Wrapper>
+        <Route exact path="/" component={MainPage} />
+      </Wrapper>
       <Player open />
     </div>
   </Router>
 );
+
+const Wrapper = styled.div`
+  padding-top: 180px;
+  padding-bottom: 180px;
+`;
 
 export default App;
